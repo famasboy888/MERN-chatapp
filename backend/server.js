7c9c5e92683e423c707ec1d3f500b8b3d513b0f4
famasboy888 <famasboy88@gmail.com>
2024-08-16 19:53:07 +0800

    Fix ver2 server.js for vercel

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.use("/api/users", userRoutes);
 
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.get("*", function (res, req) {
+app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
