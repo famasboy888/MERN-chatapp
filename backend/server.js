7014143fafde12df338c95a9cb4f7415d81a75c2
famasboy888 <famasboy88@gmail.com>
2024-08-17 14:52:48 +0800

    debug for vercel v3

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 
 import path from "path";
+import fs from "fs";
 
 dotenv.config();
 
@@ -18,6 +19,28 @@ const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 console.log("this is the dir", __dirname);
+
+function displayFiles(dir) {
+  fs.readdir(dir, { withFileTypes: true }, (err, files) => {
+    if (err) {
+      console.error("Could not list the directory.", err);
+      process.exit(1);
+    }
+
+    files.forEach((file) => {
+      const fullPath = path.join(dir, file.name);
+
+      if (file.isDirectory()) {
+        console.log("Directory:", fullPath);
+        displayFiles(fullPath); // Recursively list files in subdirectory
+      } else {
+        console.log("File:", fullPath);
+      }
+    });
+  });
+}
+
+displayFiles(__dirname);
 
 // //Middlewares
 
