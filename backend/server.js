6ddc1e42eacece89bc1016e9a09ab356574f1304
famasboy888 <famasboy88@gmail.com>
2024-08-17 14:32:50 +0800

    Debug for vercel

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,8 @@ const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
 
+console.log("this is the dir", __dirname);
+
 //Middlewares
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
@@ -27,11 +29,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+// app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+// });
 
 server.listen(PORT, () => {
   connectToMongoDB();
