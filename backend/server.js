102bf43eaab5252bb72b435079253cb696fe42ef
famasboy888 <famasboy88@gmail.com>
2024-08-17 15:40:14 +0800

    debug for vercel v7

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,6 @@ import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 
 import path from "path";
-import fs from "fs";
 
 dotenv.config();
 
@@ -18,45 +17,23 @@ const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
 
-console.log("this is the dir", __dirname);
+//Middlewares
 
-function displayFolders(dir) {
-  fs.readdir(dir, { withFileTypes: true }, (err, items) => {
-    if (err) {
-      console.error("Could not list the directory.", err);
-      process.exit(1);
-    }
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
+app.use(cookieParser());
 
-    items.forEach((item) => {
-      const fullPath = path.join(dir, item.name);
+//We will use middle ware to refrain from making this file too long
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
-      if (item.isDirectory() && item.name !== "node_modules") {
-        console.log("Directory:", fullPath);
-        displayFolders(fullPath); // Recursively list subdirectories
-      }
-    });
-  });
-}
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-displayFolders(path.join(__dirname, "frontend"));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
-// //Middlewares
-
-// app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
-// app.use(cookieParser());
-
-// //We will use middle ware to refrain from making this file too long
-// app.use("/api/auth", authRoutes);
-// app.use("/api/messages", messageRoutes);
-// app.use("/api/users", userRoutes);
-
-// // app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-// // app.get("*", (req, res) => {
-// //   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-// // });
-
-// server.listen(PORT, () => {
-//   connectToMongoDB();
-//   console.log(`Server Running on port ${PORT}`);
-// });
+server.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`Server Running on port ${PORT}`);
+});
